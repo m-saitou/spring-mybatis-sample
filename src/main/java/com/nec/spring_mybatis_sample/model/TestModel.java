6740bd8fb061088b0f627e790b8d5fd4d19bef94
SAITO Makoto <m-saito@ys.jp.nec.com>
2016-08-05 18:51:26 +0900

    add annotation

--- src/main/java/com/nec/spring_mybatis_sample/model/TestModel.java
+++ src/main/java/com/nec/spring_mybatis_sample/model/TestModel.java
@@ -2,11 +2,15 @@ package com.nec.spring_mybatis_sample.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+
 import lombok.Data;
 
 @Data
 public class TestModel {
 	private int id;
 	private String name;
+
+	@Column(name = "update_datetime")
 	private Date updateDatetime;
 }
