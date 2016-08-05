d1b1552b0756805c54955c2920e1080cbd6b31de
SAITO Makoto <m-saito@ys.jp.nec.com>
2016-08-05 18:22:07 +0900

    fix bug

--- src/main/java/com/nec/spring_mybatis_sample/mapper/TestMapper.java
+++ src/main/java/com/nec/spring_mybatis_sample/mapper/TestMapper.java
@@ -9,7 +9,7 @@ import org.apache.ibatis.annotations.Select;
 import com.nec.spring_mybatis_sample.model.TestModel;
 
 public interface TestMapper {
-	@Insert("insert into test (id, name, update_datetime) values(#{id}, #{name}, #{updateDatetime})")
+	@Insert("insert into test (name) values(#{name})")
 	public int insert(TestModel model);
 
 	@Select("select * from test where id = #{id}")
