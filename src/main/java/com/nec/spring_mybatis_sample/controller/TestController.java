e33f8e7f571bc10767c9cffc3101c73d0869e4ee
SAITO Makoto <m-saito@ys.jp.nec.com>
2016-08-05 17:33:32 +0900

    modify

--- src/main/java/com/nec/spring_mybatis_sample/controller/TestController.java
+++ src/main/java/com/nec/spring_mybatis_sample/controller/TestController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nec.spring_mybatis_sample.mapper.TesttableMapper;
@@ -23,6 +24,7 @@ public class TestController {
 
 	@RequestMapping(value = "/", method = GET)
 	@Transactional
+	@ResponseBody
 	public List<TesttableModel> show() {
 		List<TesttableModel> list = mapper.selectAll();
 		if (list == null) {
@@ -33,6 +35,7 @@ public class TestController {
 
 	@RequestMapping(value = "/", method = PUT)
 	@Transactional
+	@ResponseBody
 	public int insert(TesttableModel model) {
 		int count = mapper.selectCount() + 1;
 		model.setCount(count);
