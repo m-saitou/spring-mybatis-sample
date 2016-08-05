7700af6660c2e0d33bad6c4885db604ac4eb4553
SAITO Makoto <m-saito@ys.jp.nec.com>
2016-08-05 15:43:55 +0900

    import mybatis packages

--- src/main/java/com/nec/mapper/TesttableMapper.java
+++ src/main/java/com/nec/mapper/TesttableMapper.java
@@ -1,7 +1,11 @@
 package com.nec.mapper;
 
+import java.awt.image.SampleModel;
+
+import org.apache.ibatis.annotations.Select;
+
 public interface TesttableMapper {
-  
-  @Select("select * from testtable where id = #{id}")
-  public SampleModel select(int id);
+
+	@Select("select * from testtable where id = #{id}")
+	public SampleModel select(int id);
 }
